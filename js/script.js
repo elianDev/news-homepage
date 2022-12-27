f813c46f145066893dc1a6f4d336da6c25ae790d
Elian Silva <you@example.com>
2022-12-27 18:23:44 -0300

    menu responsivo

---
+++ js/script.js
@@ -0,0 +1,17 @@
+const btnMobile = document.querySelector("#btn-mobile");
+
+function toggleMenu(event) {
+  if (event.type === "touchstart") event.preventDefault();
+  const nav = document.querySelector("nav");
+  nav.classList.toggle("ativo");
+  const ativo = nav.classList.contains("ativo");
+  event.currentTarget.setAttribute("aria-expanded", ativo);
+  if (ativo) {
+    event.currentTarget.setAttribute("aria-label", "Fechar Menu");
+  } else {
+    event.currentTarget.setAttribute("aria-label", "Abrir Menu");
+  }
+}
+
+btnMobile.addEventListener("click", toggleMenu);
+btnMobile.addEventListener("touchstart", toggleMenu);
